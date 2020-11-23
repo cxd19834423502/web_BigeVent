62de39726280f9dd41a676a757f6e72bb8f7f1f9
cxd_1 <1123237582@qq.com>
2020-11-23 18:14:41 +0800

    大事件初始化项目2

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+$(function() {
+    // 点击去注册账号，隐藏登录区域，显示注册区域
+    $('#link_reg').on('click', function() {
+            $('.login-box').hide()
+            $('.reg-box').show()
+        })
+        // 点击去登陆，显示登录区域，隐藏注册区域
+    $('#link_login').on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+})
